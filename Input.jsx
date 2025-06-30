fc1f0c05f8dda1118d0e79c00522323068993c25
AboliChamat <95476837+AboliChamat@users.noreply.github.com>
2025-06-30 22:43:30 +0530

    Add files via upload

---
+++ Input.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+
+function Input() {
+    const[name,setName]=useState('');
+    const[number,setNumber]=useState('');
+    function handleName(e){
+        if(name.length>5){
+            alert('Length of name should be less than 5')
+        }
+        else{
+            setName(e.target.value);
+        }
+    }
+    function handleChange(e){
+        if(number>100){
+            alert('number should be less than 100');
+        }
+        else{
+            setNumber(e.target.value);
+        }
+    }
+  return (
+    <div>
+      <h1>Input</h1>
+      <input type='text' placeholder='Enter a text' onChange={(e)=>handleName(e)}/>
+      <p>{name}</p>
+      <input type='text' placeholder='Enter a number' onChange={(e)=>handleChange(e)}/>
+      <p>{number}</p>
+    </div>
+  );
+}
+
+export default Input;
