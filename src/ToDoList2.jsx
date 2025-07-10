d2784b01e05daefcd0f1d1ec6dc8689a13fc5cda
Aboli Chamat <abolichamat17@gmail.com>
2025-07-10 20:41:43 +0530

    Todolist

--- src/ToDoList2.jsx
+++ src/ToDoList2.jsx
@@ -1,38 +1,37 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 function ToDoList2() {
-    const [todo, setTodo]=useState("");
-    const [todoList, setTodoList]=useState([]);
-    const handlechange=(e)=>{
-        setTodo(e.target.value);
-    }
-    const handleclick=()=>{
-        setTodo(todo);
-        setTodoList([...todoList,todo]);
-        setTodo('');
-    }
-    const handleRemove=()=>{
-        setTodoList([]);
-    }
-    const handleDelete=(id)=>{
-        setTodoList(todoList.filter((elem,i)=>id!==i));
-    }
+  const [todo, setTodo]=useState("");
+  const [todoList, setTodoList]=useState([]);
+  const handlechange=(e)=>{
+    setTodo(e.target.value);
+  }
+  const handleclick=()=>{
+    setTodo(todo);
+    setTodoList([...todoList,todo]);
+    setTodo('');
+  }
+  const handleRemove=()=>{
+    setTodoList([]);
+  }
+  const handleDelete=(id)=>{
+    setTodoList(todoList.filter((elem,i)=> id!==i));
+  }
   return (
     <div>
       <h1>To Do List</h1>
       <div style={{display:"flex"}}>
-        <input type='text' placeholder='enter a task' value={todo} onChange={(e)=>handlechange(e)}/>
-        <button onClick={handleclick}>Add item</button>
-        <button onClick={handleRemove}>Remove All</button>
+        <input type='text' placeholder='Enter a Task' value={todo} onChange={(e)=>handlechange(e)}/>
+        <button onClick={handleclick}>Add Item</button>
+        <button onClick={handleRemove}>Delete All</button>
       </div>
       <br/>
       {todoList.map((item,id)=>{
         return(
-        <div style={{display:"flex"}}>
+        <div key={id} style={{display:"flex"}}>
         <li>{item}</li>
         <button onClick={()=>handleDelete(id)}>Delete</button>
-        </div>
-        )
+        </div>)
       })}
     </div>
   );
