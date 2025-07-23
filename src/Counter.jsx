7d139bdfae4f0627aae1005fcec08db82d4b2979
Aboli Chamat <abolichamat17@gmail.com>
2025-07-23 20:55:13 +0530

    Counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 function Counter() {
-    const[count, setCount]=useState(0);
-    const handleMinus=()=>{
-        // setCount(count-1);
-        if(count>0){
-            setCount(count-1);
-        }
+  const[count,setCount]=useState(0);
+  const handleMinus=()=>{
+    if(count>0){
+      setCount(count-1);
     }
-    const handleReset=()=>{
-        setCount(0);
-    }
+  }
+  const handleReset=()=>{
+    setCount(0);
+  }
   return (
     <div>
       <h1>Counter</h1>
