93d150459a69168cb6fad25a46bd29cf08e62aba
Aboli Chamat <abolichamat17@gmail.com>
2025-07-23 20:55:21 +0530

    Counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Todolist from './Todolist'
+import Counter from './Counter'
 
 
 
 function App() {
   return (
     <>
-    <Todolist/>
+    <Counter/>
     </>
   )
 }
